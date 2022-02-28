7b779a9c0ea1f00e936826ecea7afc64d7f1f3fc
Juicestus <jus@gtsbr.org>
2022-02-28 17:05:39 -0600

    it works!

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,53 +1,74 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.lib.GenericController;
 import frc.robot.lib.TorqueIterative;
 import frc.robot.lib.WCDDriver;
 
 public class Robot extends TorqueIterative {
-  
+
+  private final double INTAKE_SPEED = 1;
+  private final double MAG_SPEED = 1;
+  private final double LEFT_DECREASE = .6;
+
   private PWMVictorSPX frontLeft;  // Port 2
   private PWMVictorSPX frontRight; // Port 4
   private PWMVictorSPX backLeft;   // Port 1
   private PWMVictorSPX backRight; // Port 3
-  private CANSparkMax rotary;
+  private PWMVictorSPX rotary; // Port 5
+  private PWMVictorSPX magRight; // Port 6
+  private PWMVictorSPX magLeft; // Port 7
+
   
   // Algorithm for converting joystick values to motor values
   private WCDDriver wcd;
 
   private GenericController driver;
-  // private GenericController operator; Will implement later
+  private GenericController operator; 
 
   @Override
   public void robotInit() {
     wcd = new WCDDriver(.8, .6);
 
     driver = new GenericController(0, .1);
-    // operator = new GenericController(1, .1);
+    operator = new GenericController(1, .1);
 
     frontLeft = new PWMVictorSPX(3);
     frontRight = new PWMVictorSPX(1);
     backLeft = new PWMVictorSPX(4);
     backRight = new PWMVictorSPX(2);
-    rotary = new CANSparkMax(1, MotorType.kBrushless);
+    rotary = new PWMVictorSPX(5);
+    magRight = new PWMVictorSPX(6);
+    magLeft = new PWMVictorSPX(7);
   }
 
   @Override
   public void teleopContinuous() {
-    wcd.update(driver.getLeftXAxis(), -driver.getRightYAxis());
+    wcd.update(driver.getRightXAxis(), -driver.getLeftYAxis());
 
-    frontLeft.set(wcd.getLeft());
+    frontLeft.set(wcd.getLeft() * LEFT_DECREASE);
     frontRight.set(-wcd.getRight());
-    backLeft.set(wcd.getLeft());
+    backLeft.set(wcd.getLeft() * LEFT_DECREASE);
     backRight.set(-wcd.getRight());
 
-    if (driver.getAButton()) rotary.set(0.5); else rotary.set(0.5);
+    // frontLeft.set(-driver.getLeftYAxis());
+    // frontRight.set(driver.getRightYAxis());
+    // backLeft.set(-driver.getLeftYAxis());
+    // backRight.set(driver.getRightYAxis());
+
+
+    if (operator.getRightTrigger()) rotary.set(INTAKE_SPEED); 
+    else if (operator.getLeftTrigger()) rotary.set(-INTAKE_SPEED); 
+    else rotary.set(0);
+
+    if (operator.getRightTrigger()) setMag(MAG_SPEED); 
+    else if (operator.getLeftTrigger()) setMag(-MAG_SPEED); 
+    else setMag(0);
+  }
+
+  private void setMag(double speed) {
+    magRight.set(speed);
+    magLeft.set(speed);
   }
 
   // Ignore the below methods
